7b59a7e110306c148791bfd1826c168181272670
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-10-10 16:03:50 -0700

    more setup

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 5063;
 
 app.use(cors());
 app.use(express.json())
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
