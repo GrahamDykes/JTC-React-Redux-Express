088b1cb10d6cc8a0125d5f60cad4d0abb9c7610f
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-10-10 16:34:05 -0700

    why server stop working

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,25 @@ const cors = require('cors');
 // const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 5063;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
 // app.use(bodyParser.json());
 
+
+app.use((req,res)=>{
+    // console.log('Request:\n:',req)
+})
+
+
 app.get('/', (req, res) => {
+    console.log('ping detected')
     res.send('Hello from Express!');
 });
 
 app.get('/api/todos', (req,res)=>{
+    console.log('request recieved for todos')
     res.json([
         {id:1, task:'Learn React', completed:false},
         {id:2, task:'Learn Express', completed:false}
