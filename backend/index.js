7c98fbdf1e80cfe799855c6cecc5b050a14b59cb
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-10-10 15:53:33 -0700

    setting up

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5063;
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.get('/api/todos', (req,res)=>{
+    res.json([
+        {id:1, task:'Learn React', completed:false},
+        {id:2, task:'Learn Express', completed:false}
+    ])
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
